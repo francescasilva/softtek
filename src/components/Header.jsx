279194de38bc33d6568014e0b4280b38090785e2
francesca silva <frannysilva@hotmail.com>
2025-05-27 17:54:52 -0500

    se agrego header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
+
+import logo from '../assets/header/logo.png';
+import telefono from '../assets/header/telefono.png';
+
 export default function Header() {
   return (
-    <div>
-      <h1>¡Compra por este medio!</h1>
-      <h1>(01) 411 6001</h1>
+    <div className="flex items-center justify-between p-4 bg-white">
+      {/* Logo a la izquierda */}
+      <img src={logo} alt="Logo principal" className="h-12" />
       
+      {/* Contenido a la derecha */}
+      <div className="flex items-center gap-4">
+        <div>
+          <h1 className="font-bold text-sm">¡Compra por este medio!</h1>
+        </div>
+        <div className="flex items-center gap-2">
+          <img src={telefono} alt="Icono teléfono" className="h-6 w-6" />
+          <h1 className="font-bold text-xl">(01) 411 6001</h1>
+        </div>
+      </div>
     </div>
   );
 }
